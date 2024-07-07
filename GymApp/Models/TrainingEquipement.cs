6bbf410dabba4f74c092dd5f1fd38b0deed8ec5e
mihuwis <mihuwis@gmail.com>
2024-07-07 20:46:57 +0200

    Add Model classes

---
+++ GymApp/Models/TrainingEquipement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApp.Models
+{
+    public class TrainingEquipement
+    {
+        public int EquipementID { get; set; }
+        public string EquipementName { get; set; }
+    }
+}

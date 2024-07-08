e6a327c3398213ad1454224d4d97e284ee0d06ff
mihuwis <mihuwis@gmail.com>
2024-07-08 22:24:43 +0200

    Add empty statistic and training views

---
+++ GymApp/LogBookPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using GymApp.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GymApp
+{
+    /// <summary>
+    /// Interaction logic for LogBookPage.xaml
+    /// </summary>
+    public partial class LogBookPage : Page
+    {
+        private BootstrapDB _databaseFake;
+        public LogBookPage()
+        {
+            InitializeComponent();
+            _databaseFake = new BootstrapDB();
+            LoadWorkoutSessions();
+        }
+
+        private void LoadWorkoutSessions()
+        {
+            foreach (var session in _databaseFake.Workouts)
+            {
+                var sessionPanel = new StackPanel
+                {
+                    Background = Brushes.LightGreen,
+                    Margin = new Thickness(0, 5, 0, 5)
+                };
+
+                var sessionHeader = new TextBlock
+                {
+                    Text = session.TimeStarted.ToString("yyyy-MM-dd"),
+                    FontWeight = FontWeights.Bold,
+                    Margin = new Thickness(5)
+                };
+                sessionPanel.Children.Add(sessionHeader);
+
+                var exercisePanel = new StackPanel
+                {
+                    Visibility = Visibility.Collapsed,
+                };
+
+                foreach (var exerciseSet in session.ExerciseSets)
+                {
+                    var exerciseText = new TextBlock
+                    {
+                        Text = $"{exerciseSet.ExerciseExecuted.ExerciseName} - " +
+                        $"{exerciseSet.Weight}kg x " +
+                        $"{exerciseSet.Repetitions} reps",
+                        Margin = new Thickness(10, 0, 0, 0)
+                    };
+                    exercisePanel.Children.Add(exerciseText);
+                }
+
+                sessionPanel.Children.Add(exercisePanel);
+
+                sessionPanel.MouseLeftButtonUp += (sender, e) =>
+                {
+                    exercisePanel.Visibility = exercisePanel.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+                };
+
+
+                WorkoutSessionsPanel.Children.Add(sessionPanel);
+            }
+        }
+    }
+}
--- GymApp/MainWindow.xaml.cs
+++ GymApp/MainWindow.xaml.cs
@@ -18,59 +18,30 @@ namespace GymApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private BootstrapDB _databaseFake;
+
         public MainWindow()
         {
             InitializeComponent();
-            _databaseFake = new BootstrapDB();
-            LoadWorkoutSessions();
+            MainFrame.Navigate(new LogBookPage());
         }
 
-        private void LoadWorkoutSessions()
+
+        private void TrainingMenu_Click(object sender, RoutedEventArgs e)
         {
-            foreach(var session in _databaseFake.Workouts)
-            {
-                var sessionPanel = new StackPanel
-                {
-                    Background = Brushes.LightGreen,
-                    Margin = new Thickness(0, 5, 0, 5)
-                };
-
-                var sessionHeader = new TextBlock
-                {
-                    Text = session.TimeStarted.ToString("yyyy-MM-dd"),
-                    FontWeight = FontWeights.Bold,
-                    Margin = new Thickness(5)
-                };
-                sessionPanel.Children.Add(sessionHeader);
-
-                var exercisePanel = new StackPanel
-                {
-                    Visibility = Visibility.Collapsed,
-                };
-
-                foreach(var exerciseSet in session.ExerciseSets)
-                {
-                    var exerciseText = new TextBlock
-                    {
-                        Text = $"{exerciseSet.ExerciseExecuted.ExerciseName} - " +
-                        $"{exerciseSet.Weight}kg x " +
-                        $"{exerciseSet.Repetitions} reps",
-                        Margin = new Thickness(10, 0, 0, 0)
-                    };
-                    exercisePanel.Children.Add(exerciseText);
-                }
-
-                sessionPanel.Children.Add(exercisePanel);
-
-                sessionPanel.MouseLeftButtonUp += (sender, e) =>
-                {
-                    exercisePanel.Visibility = exercisePanel.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
-                };
-
-
-                WorkoutSessionsPanel.Children.Add(sessionPanel);
-            }
+            MainFrame.Navigate(new TrainingPage());
         }
+
+        private void LogBookMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LogBookPage());
+        }
+
+        private void StatisticsMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new StatisticsPage());
+        }
+
+
+    
     }
 }

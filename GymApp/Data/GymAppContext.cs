0a9de6dadae92ad2c64cef255f942cd6db7a5c79
mihuwis <mihuwis@gmail.com>
2024-07-12 18:14:23 +0200

    Add context class

---
+++ GymApp/Data/GymAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GymApp.Models;
+
+namespace GymApp.Data
+{
+    class GymAppContext : DbContext
+    {
+        public GymAppContext(DbContextOptions<GymAppContext> options) : base(options) { }
+
+        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseSet> ExerciseSets { get; set; }
+        public DbSet<TrainingEquipement> TrainingEquipements { get; set; }
+        public DbSet<BodyPart> BodyParts { get; set; }
+    }
+}

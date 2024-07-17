1980fd0460fcb4d76f8e1fe472b09214a177a21f
mihuwis <mihuwis@gmail.com>
2024-07-17 17:49:02 +0200

    Add testing. First commit

---
+++ GymAppTests/LogBookPageTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GymApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymApp.Context;
+using Moq;
+using GymApp.Models;
+
+
+namespace GymApp.Tests
+{
+    [TestClass()]
+    public class LogBookPageTests
+    {
+        private Mock<BootstrapDB> _mockDatabase;
+        private LogBookPage _logBookPage;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Mock the database
+            _mockDatabase = new Mock<BootstrapDB>();
+
+            // Setup fake workout data
+            var fakeWorkouts = new List<WorkoutSession>
+            {
+                new WorkoutSession
+                {
+                    TimeStarted = DateTime.Now.AddDays(-1),
+                    ExerciseSets = new List<ExerciseSet>
+                    {
+                        new ExerciseSet { Exercise = new Exercise { ExerciseName = "Squats" }, Weight = 100, Repetitions = 10 },
+                        new ExerciseSet { Exercise = new Exercise { ExerciseName = "Bench Press" }, Weight = 80, Repetitions = 8 }
+                    }
+                },
+                new WorkoutSession
+                {
+                    TimeStarted = DateTime.Now.AddDays(-2),
+                    ExerciseSets = new List<ExerciseSet>
+                    {
+                        new ExerciseSet { Exercise = new Exercise { ExerciseName = "Deadlift" }, Weight = 120, Repetitions = 5 },
+                    }
+                }
+            };
+
+            _mockDatabase.Setup(db => db.Workouts).Returns(fakeWorkouts);
+
+            // Create the LogBookPage instance with the mocked database
+            _logBookPage = new LogBookPage(_mockDatabase.Object);
+        }
+
+        [TestMethod()]
+        public void LoadWorkoutSessionsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
